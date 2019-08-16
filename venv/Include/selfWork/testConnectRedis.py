bbf06d92c9875090348ea7c38860bc8c435e4a32
joker-syc <2985759264@qq.com>
2019-08-16 14:41:41 +0800

    删除了不必要的数据,新增了练习代码

--- venv/Include/selfWork/testConnectRedis.py
+++ venv/Include/selfWork/testConnectRedis.py
@@ -8,17 +8,43 @@
 
 import redis
 
-# r=redis.Redis(password='123456',decode_responses=True)
-# r.set('s','ss')
-# print(r.get('s'))
-
-#创建连接池
-pool=redis.ConnectionPool(password='123456',decode_responses=True)
-
-#使用连接池对象去连接redis数据库
-r=redis.Redis(connection_pool=pool)
-
-# r.delete('s')
-r.set('s','sss')
-r.delete('s')
+r=redis.Redis(password='123456',decode_responses=True)
+r.set('s','ss')
 print(r.get('s'))
+print(r.ttl('s'))
+r.expire('s',10)
+print(r.ttl('s'))
+r.persist('s')
+print(r.ttl('s'))
+r.delete('s')
+
+r.lpush('dd','?','??','???')
+print(r.lrange('dd',0,-1))
+print(r.lindex('dd',1))
+r.lpop('dd')
+r.lpush('dd','????')
+print(r.lrange('dd',0,-1))
+r.delete('dd')
+
+r.hset('zz',1,'2')
+print(r.hget('zz',1))
+r.hmset('zz',{3:'4',5:'6'})
+# r.hmset('zz',['a','b','c','d'])        #必须是成对出现的
+# r.hmset('zz',[['a','b'],['c','d']])        #必须是成对出现的  看来列表是不行的
+print(r.hgetall('zz'))
+print(r.hexists('zz',6))
+print(r.hexists('zz',1))
+
+r.delete('zz')
+
+
+# #创建连接池
+# pool=redis.ConnectionPool(password='123456',decode_responses=True)
+#
+# #使用连接池对象去连接redis数据库
+# r=redis.Redis(connection_pool=pool)
+#
+# # r.delete('s')
+# r.set('s','sss')
+# r.delete('s')
+# print(r.get('s'))
--- venv/Include/selfWork/tryToConnectMysql.py
+++ venv/Include/selfWork/tryToConnectMysql.py
@@ -11,12 +11,51 @@ import pymysql
 #1.创建数据库连接对象
 conn=pymysql.connect(host='localhost',user='root',password='123456')
 #2.选择操控的数据库
-conn.select_db('school')
+conn.select_db('test')
 #3.设置连接输出结果的字符集
 conn.set_charset('utf8')
 #4.创建游标对象(方便遍历执行sql操作后返回的结果集)
 cursor=conn.cursor()
 #5.初始化sql操作(sql语句)
+sql='select * from student'
+# #6.执行要执行的sql语句
+# cursor.execute(sql)              #一般不用这种方式,而是要用一个变量接收
+# #7.接收并处理结果集
+# print(cursor.fetchone())
+#
+# print(cursor.fetchall())
+
+#6.执行要执行的sql语句
+dd=cursor.execute(sql)
+#7.接收并处理结果集
+print(dd)         #这里打印的是结果集中的元素数量
+print(cursor.rowcount)
+print(cursor.rownumber)
+
+print(cursor.fetchone())
+print(cursor.rownumber)
+print(cursor.fetchall())
+
+# cursor.scroll()
+print(cursor.fetchone())
+print(cursor.rownumber)
+print(cursor.fetchall())
+print(cursor.rownumber)
+
+conn.begin()
+try:
+    sql='select * from student where ssex="男"'
+    a=cursor.execute(sql)
+except:
+    conn.rollback()
+else:
+    print('dd')
+    print(cursor.fetchall())
+    conn.commit()
+#8.关闭游标
+cursor.close()
+#9.关闭数据库连接
+conn.close()
 
 
 
