54a2b4558669c109898500f5cf2e0b191758ad41
joker-syc <2985759264@qq.com>
2019-08-17 08:45:13 +0800

    更新了numpy的学习进程

--- venv/Include/selfStudy/numpy/try1.py
+++ venv/Include/selfStudy/numpy/try1.py
@@ -42,9 +42,10 @@ import numpy as np
 #             [1,3,4,4,4,5,5,6,6],
 #             [1,3,4,4,4,5,5,6,6],
 #             [1,3,4,4,4,5,5,6,6],
-#             [1,3,4,4,4,5,5,6,[1,2]]
-#            ])
-#如果已经预先设置限定存储的类型,一级数组中出现了类型不一致的情况时,会在创建ndarray对象时报错
+#             [1,3,4,4,4,5,5,6,'4']
+#            ],int)
+# #如果已经预先设置限定存储的类型,一级数组中出现了类型不一致的情况时,会在创建ndarray对象时报错
+# # (实际上,只有当数组内的元素是无法通过相应得转化得到限定的存储类型时才会报错,其他时候会自动转换)
 # print(a)
 # print(len(a))
 # print(a.shape)          #看来必须是非等长数组才能
@@ -55,16 +56,51 @@ import numpy as np
 # print(a.flags)
 
 # import time
-# while 1:
-#     time.sleep(1)
-#     a=np.empty([3,3])     #此方法创建的是一个未初始化数组    里面的数据在未初始化之前是随机的
-#     print(a)
-#     print(id(a))
-
-# a=[[1,2,3],
-#    [4,5,6],
-#    [7,8,9]]
+# # while 1:
+# time.sleep(1)
+# # a=np.empty([3,3])     #此方法创建的是一个未初始化数组    里面的数据在未初始化之前是随机的
+# # a=np.zeros([4,4])     #此方法创建的是一个数组元素全是0 的数组
+# a=np.ones([5,5])       #此方法创建的是一个数组元素全是1 的数组
+#
 # print(a)
-# print(type(a))
-# print(id(a))
-#很明显,两个a根本不是一个a,可以得出结论,empty创建的数组是无法重新赋值的
+# # print(id(a))          #每次调用np.empty()都会开辟新的内存空间
+#
+# z=1
+# for x in range(0,3):
+#     for y in range(0,3):
+#         a[x][y]=z
+#         z+=1
+# print(a)               #可以通过下标给数组内存储的值进行初始化
+
+
+# a=[[1,2,3,4],
+#    [1,2,3,4],
+#    [1,2,3,'4']]
+# b=np.asarray(a,int)
+# print(a)
+# print(b)
+
+# a=b'jsudjhiljieji'
+# # b=np.frombuffer(a,dtype='S1')
+# # b=np.frombuffer('skdjj2jkj23j',dtype='S1')          #看来只能处理同一类型的数据
+# b=np.frombuffer(b'\1\2\3\4\5\6\7',dtype=np.uint8)    #搞不懂
+#
+# '''
+# >> > s = 'hello world'
+# >> > np.frombuffer(s, dtype='S1', count=5, offset=6)
+# array(['w', 'o', 'r', 'l', 'd'],
+#       dtype='|S1')                                      #可以看出来'S1'是用来处理字符型
+#
+# >> > np.frombuffer(b'\x01\x02', dtype=np.uint8)
+# array([1, 2], dtype=uint8)
+# >> > np.frombuffer(b'\x01\x02\x03\x04\x05', dtype=np.uint8, count=3)
+# array([1, 2, 3], dtype=uint8)
+# '''
+# print(b)
+
+# a=np.arange(5)
+# a=np.arange(1,10,2,dtype=float)      #(start,stop,step)
+# print(a)
+
+a=np.linspace()
+print(a)
--- venv/Include/selfWork/testConnectRedis.py
+++ venv/Include/selfWork/testConnectRedis.py
@@ -7,7 +7,7 @@
 # @Software: PyCharm
 
 import redis
-
+print('string类型的使用')
 r=redis.Redis(password='123456',decode_responses=True)
 r.set('s','ss')
 print(r.get('s'))
@@ -17,15 +17,17 @@ print(r.ttl('s'))
 r.persist('s')
 print(r.ttl('s'))
 r.delete('s')
-
+print('列表的使用')
 r.lpush('dd','?','??','???')
 print(r.lrange('dd',0,-1))
 print(r.lindex('dd',1))
 r.lpop('dd')
 r.lpush('dd','????')
-print(r.lrange('dd',0,-1))
+print(r.lrange('dd',-2,-1))
+print(r.lindex('dd','2'))
+print(r.llen('dd'))
 r.delete('dd')
-
+print('哈希类型的使用')
 r.hset('zz',1,'2')
 print(r.hget('zz',1))
 r.hmset('zz',{3:'4',5:'6'})
@@ -34,8 +36,18 @@ r.hmset('zz',{3:'4',5:'6'})
 print(r.hgetall('zz'))
 print(r.hexists('zz',6))
 print(r.hexists('zz',1))
-
+print(r.hkeys('zz'))
+print(r.hvals('zz'))
+r.hdel('zz','3')
 r.delete('zz')
+print('有序集合的使用')
+r.zadd('dd',{'/':1,'//':2})
+print(r.zcard('dd'))
+r.zadd('dd',{'///':4,'////':3})
+print(r.zrange('dd',0,-1))          #根据分数进行升序排序
+print(r.zrevrange('dd',0,-1))       #根据分数进行降序排序
+print(r.zscore('dd','/'))           #返回指定元素的分数
+r.delete('dd')
 
 
 # #创建连接池
